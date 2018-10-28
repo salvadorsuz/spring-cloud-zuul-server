bed72c84b01ce0ea0190ba5d943d0b9a19fe2bf5
salvadorsuz <salvadorsuz@yahoo.es>
2018-10-28 20:05:28 +0100

    Initial commit

---
+++ src/main/java/com/in28minutes/microservices/springcloudzuulserver/SpringCloudZuulServerApplication.java
@@ -0,0 +1,12 @@
+package com.in28minutes.microservices.springcloudzuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudZuulServerApplication.class, args);
+	}
+}

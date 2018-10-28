9cccd49039439f9920b1d57f249fccc00f1bc98f
salvadorsuz <salvadorsuz@yahoo.es>
2018-10-28 21:02:49 +0100

    Filter request

--- src/main/java/com/in28minutes/microservices/springcloudzuulserver/SpringCloudZuulServerApplication.java
+++ src/main/java/com/in28minutes/microservices/springcloudzuulserver/SpringCloudZuulServerApplication.java
@@ -2,8 +2,12 @@ package com.in28minutes.microservices.springcloudzuulserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
 public class SpringCloudZuulServerApplication {
 
 	public static void main(String[] args) {

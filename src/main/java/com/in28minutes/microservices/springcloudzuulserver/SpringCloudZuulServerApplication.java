9ac799a0d1017ec02d0645229f4d52ea166a6cf6
salvadorsuz <salvadorsuz@yahoo.es>
2018-12-20 21:22:41 +0100

    rabbit zipkin  logback config

--- src/main/java/com/in28minutes/microservices/springcloudzuulserver/SpringCloudZuulServerApplication.java
+++ src/main/java/com/in28minutes/microservices/springcloudzuulserver/SpringCloudZuulServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,5 +15,10 @@ public class SpringCloudZuulServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudZuulServerApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 }
